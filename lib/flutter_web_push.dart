97f1af544d1daa7f8af7847691d2bf6cb9044b5c
unknown <izmurzenov@gmail.com>
2025-03-05 00:32:38 +0700

    save for checkout

--- lib/flutter_web_push.dart
+++ lib/flutter_web_push.dart
@@ -1,57 +1,19 @@
-library;
-
-import 'dart:html' as html;
-import 'dart:js_util' as js_util;
+import 'dart:js' as js;
 
 class FlutterWebPush {
-  static FlutterWebPush? _instance;
-  late html.ServiceWorkerRegistration _swRegistration;
-
-  FlutterWebPush._();
-
-  static Future<FlutterWebPush> initialize(String serviceWorkerPath) async {
-    if (_instance != null) return _instance!;
-
-    final registration = await html.window.navigator.serviceWorker?.register(
-      serviceWorkerPath,
-    );
-    if (registration == null) {
-      throw Exception(
-        "Service Worker не поддерживается или регистрация не удалась.",
-      );
-    }
-
-    _instance = FlutterWebPush._();
-    _instance!._swRegistration = registration;
-    return _instance!;
+  /// Инициализация Web Push
+  static Future<void> initialize() async {
+    js.context.callMethod('initializeWebPush');
   }
 
-  Future<html.PushSubscription?> subscribe(String vapidKey) async {
-    final permission = await html.Notification.requestPermission();
-    if (permission != 'granted') {
-      throw Exception("Уведомления не разрешены пользователем.");
-    }
-
-    final options = js_util.jsify({
-      "userVisibleOnly": true,
-      "applicationServerKey": vapidKey,
-    });
-
-    final subscription = await js_util.promiseToFuture<html.PushSubscription?>(
-      js_util.callMethod(_swRegistration.pushManager as Object, 'subscribe', [
-        options,
-      ]),
-    );
-
-    return subscription;
+  /// Подписка на уведомления, возвращает subscription объект
+  static Future<String?> subscribe() async {
+    final subscription = js.context.callMethod('subscribeToPush');
+    return subscription as String?;
   }
 
-  Future<void> unsubscribe() async {
-    final subscription = await _swRegistration.pushManager?.getSubscription();
-    await subscription?.unsubscribe();
-  }
-
-  Future<html.PushSubscription?> getSubscription() async {
-    return await _swRegistration.pushManager?.getSubscription();
+  /// Отписка от Web Push
+  static Future<void> unsubscribe() async {
+    js.context.callMethod('unsubscribeFromPush');
   }
 }
