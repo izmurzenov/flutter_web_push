5610534327ab692d5ba105ff3a09d5992e6a7d9b
unknown <izmurzenov@gmail.com>
2025-03-05 00:53:39 +0700

    save for checkout

--- lib/flutter_web_push.dart
+++ lib/flutter_web_push.dart
@@ -1,30 +1,10 @@
+import 'dart:js_interop';
+
 @JS()
-library flutter_web_push;
-
-import 'dart:async';
-import 'dart:convert';
-import 'package:js/js.dart';
-
-@JS('webPush.init')
-external void jsInit(String serviceWorkerPath, String publicVapidKey);
-
-@JS('webPush.subscribe')
-external String jsSubscribe();
-
-@JS('webPush.unsubscribe')
-external bool jsUnsubscribe();
+external void subscribeToPush();
 
 class FlutterWebPush {
-  static Future<void> init(String serviceWorkerPath, String publicVapidKey) async {
-    jsInit(serviceWorkerPath, publicVapidKey);
-  }
-
-  static Future<Map<String, dynamic>> subscribe() async {
-    final jsonStr = jsSubscribe();
-    return jsonDecode(jsonStr);
-  }
-
-  static Future<bool> unsubscribe() async {
-    return jsUnsubscribe();
+  static void subscribe() {
+    subscribeToPush();
   }
 }
